08a0d269a6d95973406b9fe9b620361988148850
imkrish7 <singhkrishk07@gmail.com>
2025-03-11 23:42:56 +0530

    stripe payment needs to integrate

--- src/app/(shop)/orders/_layout.tsx
+++ src/app/(shop)/orders/_layout.tsx
@@ -1,8 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
+import { useOrderUpdate } from '../../api/subscription'
 
 const OrdersLayout = () => {
+  useOrderUpdate();
   return (
   <Stack>
     <Stack.Screen  name="index" options={{headerShown: false}}/>
